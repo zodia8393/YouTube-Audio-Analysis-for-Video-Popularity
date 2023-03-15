4961aadde1ed75d699ff848795d6b5af0922dff7
HJ <83521479+zodia8393@users.noreply.github.com>
2023-03-15 09:53:30 +0900

    Create results_analysis.py

---
+++ results_analysis.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+
+def plot_training_history(history):
+    # Plot training & validation accuracy values
+    plt.plot(history.history['accuracy'])
+    plt.plot(history.history['val_accuracy'])
+    plt.title('Model accuracy')
+    plt.ylabel('Accuracy')
+    plt.xlabel('Epoch')
+    plt.legend(['Train', 'Validation'], loc='upper left')
+    plt.show()
+
+    # Plot training & validation loss values
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('Model loss')
+    plt.ylabel('Loss')
+    plt.xlabel('Epoch')
+    plt.legend(['Train', 'Validation'], loc='upper left')
+    plt.show()
